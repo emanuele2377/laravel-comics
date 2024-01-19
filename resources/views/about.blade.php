963a413bc34646228b700b2bb85bed5afcb951cf
Your Name <you@example.com>
2024-01-19 19:01:42 +0100

    funzionante

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,25 +3,25 @@
 @section("title", "About")
 
 @section('content')
-<h1>About us: {{ $titolo }}</h1>
-<p>{{ $desc }}</p>
+    <h1>About us: {{ $titolo }}</h1>
+    <p>{{ $desc }}</p>
+    
+    <h2>Studenti</h2>
+    <ul>
+        @foreach ($students as $student)
+            <li>{{ $student }}</li>
+        @endforeach
+    </ul>
 
-<h2>Studenti</h2>
-<ul>
-    @foreach ($students as $student)
-    <li>{{ $student }}</li>
-    @endforeach
-</ul>
-
-<h2>Docenti</h2>
-@if (count($teachers)>0)
-<ul>
-    @foreach ($teachers as $teacher)
-    <li>{{ $teacher }}</li>
-    @endforeach
-</ul>
-@else
-<p>Non sono ancora presenti docenti</p>
-@endif
-
+    <h2>Docenti</h2>
+        @if (count($teachers)>0)
+            <ul>
+            @foreach ($teachers as $teacher)
+                <li>{{ $teacher }}</li>
+            @endforeach
+            </ul>
+        @else
+            <p>Non sono ancora presenti docenti</p>
+        @endif
+        
 @endsection
